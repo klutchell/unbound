eca35446797c7d4f158a8fba52f5595b411bf5db
Kyle Harding <kylemharding@gmail.com>
2018-10-06 14:31:38 +0000

    initial support for unbound

--- start.sh
+++ start.sh
@@ -4,8 +4,5 @@
 # eg. TZ=America/Toronto
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
-if [ "${INITSYSTEM}" != "on" ]
-then
-	# start unbound daemon
-	/usr/bin/unbound
-fi
+# start unbound daemon
+/usr/sbin/unbound -d -c /etc/unbound/unbound.conf.d/pi-hole.conf
