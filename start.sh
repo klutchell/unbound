10ed4040ab7b9f77216a0fcb1f87b3a5111250c8
Kyle Harding <kylemharding@gmail.com>
2019-01-13 13:18:31 -0500

    take ownership of unbound lib dir

--- start.sh
+++ start.sh
@@ -13,5 +13,9 @@ ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 # doing it more often then every 6 months.
 curl https://www.internic.net/domain/named.root -o "/var/lib/unbound/root.hints"
 
+# take full ownership of unbound lib dir since the unbound process needs write access
+# fatal error: could not open autotrust file for writing, /var/lib/unbound/root.key.17-0: Permission denied
+chown -R unbound:unbound "/var/lib/unbound"
+
 # start unbound daemon
 /usr/sbin/unbound -d -c "/etc/unbound/unbound.conf.d/default.conf"
