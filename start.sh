08df386d23daf24b47e04fe7dfb8dcc238c3055f
Kyle Harding <kylemharding@gmail.com>
2019-01-15 16:55:49 -0500

    take ownership of config dir

--- start.sh
+++ start.sh
@@ -5,17 +5,18 @@
 ln -snf /usr/share/zoneinfo/$TZ /etc/localtime && echo $TZ > /etc/timezone
 
 # update the root anchor for dnssec
+echo "Update root trust anchor for DNSSEC validation..."
 /opt/unbound/sbin/unbound-anchor -a "/opt/unbound/etc/unbound/root.key"
 
 # Download the list of primary root servers.
 # Unbound ships its own list but we can also download the most recent list and
 # update it whenever we think it is a good idea. Note: there is no point in
 # doing it more often then every 6 months.
+echo "Download the list of primary root servers..."
 curl https://www.internic.net/domain/named.root -o "/opt/unbound/etc/unbound/root.hints"
 
-# take full ownership of unbound lib dir since the unbound process needs
-# write access to the root.key parent dir
-# chown -R unbound:unbound "/var/lib/unbound"
+echo "Take ownership of the unbound configuration directory..."
+chown -R unbound:unbound "/opt/unbound/etc/unbound"
 
-# start unbound daemon
+echo "Start unbound daemon..."
 /opt/unbound/sbin/unbound -d -c "/opt/unbound/etc/unbound/unbound.conf"
