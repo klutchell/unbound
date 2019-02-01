a099df3f909289fcba1f98dbafbd737478e2d538
Kyle Harding <kylemharding@gmail.com>
2019-02-01 08:52:21 -0500

    shellcheck changes

--- start.sh
+++ start.sh
@@ -5,7 +5,7 @@
 if [ -n "${TZ}" ]
 then
 	echo "Setting timezone to ${TZ}..."
-	ln -snf /usr/share/zoneinfo/${TZ} /etc/localtime && echo ${TZ} > /etc/timezone
+	ln -snf "/usr/share/zoneinfo/${TZ}" /etc/localtime && echo "${TZ}" > /etc/timezone
 	echo "Done."
 fi
 
