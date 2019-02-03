b144f2cc97c6a9784f7aa0a1a2bf9d85f636f092
Kyle Harding <kylemharding@gmail.com>
2019-02-03 16:45:52 -0500

    fix comments

--- tests/start.sh
+++ tests/start.sh
@@ -1,15 +1,9 @@
 #!/bin/sh
 
-# for use with alpine linux only
-
-# 1. install dig via bind-tools
-# 2. test unbound dns response to sigfail.verteiltesysteme.net
-# 3. fail if response does not include SERVFAIL
-# 4. test unbound dns response to sigok.verteiltesysteme.net
-# 5. fail if response does not include NOERROR
-
 sleep 5
 
+# test unbound dns response to sigfail.verteiltesysteme.net
+# fail if response does not include SERVFAIL
 echo "Testing sigfail.verteiltesysteme.net..."
 if dig "sigfail.verteiltesysteme.net" @unbound -p 53 | grep -q "SERVFAIL"
 then
@@ -19,6 +13,8 @@ else
 	exit 1
 fi
 
+# test unbound dns response to sigok.verteiltesysteme.net
+# fail if response does not include NOERROR
 echo "Testing sigok.verteiltesysteme.net..."
 if dig "sigok.verteiltesysteme.net" @unbound -p 53 | grep -q "NOERROR"
 then
