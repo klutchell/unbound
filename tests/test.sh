998129f1ca8a0de260f126ca6906b146b5b5a594
Kyle Harding <kylemharding@gmail.com>
2019-02-08 14:26:41 -0500

    make tests verbose

--- tests/test.sh
+++ tests/test.sh
@@ -10,3 +10,5 @@ dig "sigfail.verteiltesysteme.net" @unbound -p 53 | grep -q "SERVFAIL" || exit 1
 # test unbound dns response to sigok.verteiltesysteme.net
 # fail if response does not include NOERROR
 dig "sigok.verteiltesysteme.net" @unbound -p 53 | grep -q "NOERROR" || exit 1
+
+exit 0
