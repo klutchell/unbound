756da1059a36a4dfe3610f45a4b646b57cbe999b
Kyle Harding <kylemharding@gmail.com>
2019-02-12 19:45:10 -0500

    update tests

--- tests/test.sh
+++ tests/test.sh
@@ -5,10 +5,10 @@ sleep 5
 
 # test unbound dns response to sigfail.verteiltesysteme.net
 # fail if response does not include SERVFAIL
-dig "sigfail.verteiltesysteme.net" @unbound -p 53 | grep -q "SERVFAIL" || exit 1
+drill @unbound "sigfail.verteiltesysteme.net" | grep -q "SERVFAIL" || exit 1
 
 # test unbound dns response to sigok.verteiltesysteme.net
 # fail if response does not include NOERROR
-dig "sigok.verteiltesysteme.net" @unbound -p 53 | grep -q "NOERROR" || exit 1
+drill @unbound "sigok.verteiltesysteme.net" | grep -q "NOERROR" || exit 1
 
 exit 0
