a01ad10d9ce161b47a4276ece79affdb05158174
Kyle Harding <kmharding@pm.me>
2019-09-22 14:23:11 -0400

    remove old directory conf.d

--- start.sh
+++ start.sh
@@ -1,11 +1,10 @@
 #!/bin/sh
 
-mkdir -p /opt/unbound/etc/unbound/conf.d 2>/dev/null
 mkdir -p /opt/unbound/etc/unbound/dev 2>/dev/null
-mkdir -p -m 700 /opt/unbound/etc/unbound/var 2>/dev/null
-
-chown _unbound:_unbound /opt/unbound/etc/unbound/var
 cp -a /dev/random /dev/urandom /opt/unbound/etc/unbound/dev/
+
+mkdir -p -m 700 /opt/unbound/etc/unbound/var 2>/dev/null
+chown _unbound:_unbound /opt/unbound/etc/unbound/var
 
 # update the root trust anchor for DNSSEC validation
 /opt/unbound/sbin/unbound-anchor -a /opt/unbound/etc/unbound/var/root.key
